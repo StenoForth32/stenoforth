124c75fd5e7cbe685ee75b7b9204d1795d9e86ad
StenoForth32 <vv.maslov.52@mail.ru>
2025-01-15 15:00:55 +0300

    Add files via upload

--- ~stenoforth/lib/man-ip.f
+++ ~stenoforth/lib/man-ip.f
@@ -9,15 +9,15 @@ m: I+  ( n -- )  R@ + RP@ ! ; \ макрос - увеличение счетчи
 
 : sqrt ( n -- sqrt ) C=A $ -4 @P=A $ -4 0=@P 0SQRT $ -4 @P=0- $ -4 A=@P B=A *A C=A? L1 J>= B-- L1: A=B ;
 
-: D* D>F D>F F* F>D ;
-: D/ 2SWAP D>F D>F F/ F>D ;
+: D*  D>F D>F F* F>D ;
+: D/  2SWAP D>F D>F F/ F>D ;
 
-m: DOR     ( d1 d2 -- d3)  D=@P $ 8 Pa $ -4 A|@P @P|D ;
-m: DXOR    ( d1 d2 -- d3)  D=@P $ 8 Pa $ -4 A^@P @P^D ;
-m: DAND    ( d1 d2 -- d3)  D=@P $ 8 Pa $ -4 A&@P @P&D ;
-m: DINVERT ( d1 -- d2 )    @P~ A~ ;
-m: DLSHIFT ( d n -- 'd ) $ 4 D=@P $ 20 C=# C-A D>> C=A @P<< $ 4 @P<< @P|D DROP ;
-m: DRSHIFT ( d n -- 'd ) D=@P $ 20 C=# C-A D<< C=A @P>> $ 4 @P>> $ 4 @P|D DROP ;
+: DOR     ( d1 d2 -- d3)  D=@P $ 8 Pa $ -4 A|@P @P|D ;
+: DXOR    ( d1 d2 -- d3)  D=@P $ 8 Pa $ -4 A^@P @P^D ;
+: DAND    ( d1 d2 -- d3)  D=@P $ 8 Pa $ -4 A&@P @P&D ;
+: DINVERT ( d1 -- d2 )    @P~ A~ ;
+: DLSHIFT ( d n -- 'd ) $ 4 D=@P $ 20 C=# C-A D>> C=A @P<< $ 4 @P<< @P|D DROP ;
+: DRSHIFT ( d n -- 'd ) D=@P $ 20 C=# C-A D<< C=A @P>> $ 4 @P>> $ 4 @P|D DROP ;
 : D0<>     ( d -- f ) D0= INVERT ;
-m: -!      ( n addr -- ) D=@P -D @A+D $ 4 A=@P $ 8 Pa  ;
-m: 1-!     ( addr --) @A-- DROP ;
+: 1-!     ( addr --) @A-- DROP ;
+: -!      ( n addr -- ) D=@P -D @A+D $ 4 A=@P $ 8 Pa ;
