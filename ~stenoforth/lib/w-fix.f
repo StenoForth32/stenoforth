fe62377a05f66e81d179a3a19c35ac2d531a97b1
StenoForth32 <vv.maslov.52@mail.ru>
2025-01-22 22:59:35 +0300

    Add files via upload

--- ~stenoforth/lib/w-fix.f
+++ ~stenoforth/lib/w-fix.f
@@ -1,12 +1,13 @@
 \ stenoforth32
 
+\ перевод часто используемых имен слов в нижний регистр 
+
 m: c@ C@ ;
 m: w@ W@ ;
 m: c! C! ;
 m: w! W! ;
 m: c, C, ;
 m: w, W, ;
-
 m: dup DUP ;
 m: 2dup 2DUP ;
 m: drop DROP ;
@@ -40,32 +41,27 @@ m: cr CR ;
 m: hex HEX ;
 m: decimal DECIMAL ;
 m: binary BINARY ;
-
 m: or OR ;
 m: xor XOR ;
 m: and AND ;
 m: invert INVERT ;
 m: lshift LSHIFT ;
 m: rshift RSHIFT ;
-
 m: if IF ;
 m: else ELSE ;
 m: then THEN ;
 m: true TRUE ;
 m: false FALSE ;
-
 m: case CASE ;
 m: of OF ;
 m: endof ENDOF ;
 m: endcase ENDCASE ;
-
 m: begin BEGIN ;
 m: again AGAIN ;
 m: until UNTIL ;
 m: while WHILE ;
 m: repeat REPEAT
 m: exit EXIT ;
-
 m: do DO ;
 m: ?do ?DO ;
 m: aDO ado ;
@@ -74,7 +70,6 @@ m: leave LEAVE ;
 m: unloop UNLOOP ;
 m: loop LOOP ;
 m: +loop +LOOP ;
-
 m: create CREATE ;
 m: aligned ALIGNED ;
 m: align ALIGN ;
@@ -110,32 +105,26 @@ m: end-code END-CODE ;
 m: ret, RET, ;
 m: :noname :NONAME ;
 m: to TO ;
-
 m: erase ERASE ;
 m: fill FILL ;
 m: compare COMPARE ;
 m: search SEARCH ;
 m: move MOVE ;
-
 m: emit EMIT ;
 m: key KEY ;
-
 m: within WITHIN ;
 m: min MIN ;
 m: max MAX ;
-
 m: abs ABS ;
 m: negate NEGATE ;
 m: mod MOD ;
 m: /mod /MOD ;
-
 m: evaluate EVALUATE ;
 m: compile, COMPILE, ;
 m: [compile] [COMPILE] ;
 m: postpone POSTPONE ;
 m: execute EXECUTE ;
 m: immediate IMMEDIATE ;
-
 m: base BASE ;
 m: state STATE ;
 I: s" ` S" ;
